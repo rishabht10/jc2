739e43910d013af09f4076711ae20cae3474649b
Rishabh Tiwari <110122658+rishabht10@users.noreply.github.com>
2022-12-22 22:54:33 +0530

    Add files via upload

---
+++ jc.js
@@ -0,0 +1,34 @@
+let subbtn = document.getElementById("q4");
+        subbtn.addEventListener("click", prtSub);
+        function prtSub() {
+           alert("Query Submitted");
+        } 
+
+let cont=document.getElementById("divcont");
+
+
+cont.addEventListener("mouseover",()=>{
+   document.getElementById("navbr").classList.add("disno");
+
+})
+
+cont.addEventListener("mouseleave",()=>{
+
+   document.getElementById("navbr").classList.remove("disno");   
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
